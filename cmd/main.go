e35131eaf0a2a456f7161c9f834654a51f16c3a5
Santiago Canal <santycanal_06@hotmail.com>
2024-06-14 14:53:30 -0500

    finali the proyecy of Gsender

--- cmd/main.go
+++ cmd/main.go
@@ -31,6 +31,7 @@ func main() {
 	ins_log.SetService("EpinMailSender")
 	ins_log.SetLevel(config.Config.LogLevel)
 	ctx = ins_log.SetPackageNameInContext(ctx, "main")
+	defer end(ctx)
 
 	ins_log.Infof(ctx, "starting email sender gateway version: %+v", getVersion())
 	// Ejecutamos la función para enviar correos electrónicos
@@ -115,7 +116,8 @@ func (a *LoginAuth) Next(fromServer []byte, more bool) ([]byte, error) {
 }
 func sendEmails(ctx context.Context) {
 	for {
-
+		utfi := ins_log.GenerateUTFI()
+		ctx = ins_log.SetUTFIInContext(ctx, utfi)
 		// Calcular la hora específica del día para repetir las tareas
 		dailyTime := config.Config.ExecutionTime
 
@@ -131,6 +133,7 @@ func sendEmails(ctx context.Context) {
 		durationUntilNextExecution := nextExecution.Sub(now)
 
 		// Esperar hasta la próxima ejecución
+		ins_log.Tracef(ctx, "durayion Until Next Execution: %s", durationUntilNextExecution)
 		time.Sleep(durationUntilNextExecution)
 
 		smtpHost := config.Config.SmtpConfig.SmtpHost
@@ -138,9 +141,11 @@ func sendEmails(ctx context.Context) {
 		senderEmail := config.Config.MailInfo.MailSender
 		subject := config.Config.MailInfo.Subject
 
+		ins_log.Infof(ctx, "this are the emails to send")
 		// Obtener todos los correos electrónicos de los destinatarios
 		var recipients []string
-		for _, receiver := range config.Config.MailInfo.MailReceivers {
+		for i, receiver := range config.Config.MailInfo.MailReceivers {
+			ins_log.Infof(ctx, "%s: %s", i, receiver.Email)
 			recipients = append(recipients, receiver.Email)
 		}
 
@@ -148,76 +153,74 @@ func sendEmails(ctx context.Context) {
 		// Adjuntar el archivo PDF
 
 		// Obtener la fecha de ayer
-		yesterday := time.Now().AddDate(0, 0, -1)
-
-		// Formatear la fecha como "YYYYMMDD"
-		yesterdayFormatted := yesterday.Format("20060102")
+		today := time.Now()
+		todayFormatted := today.Format("20060102")
 
 		// Concatenar el nombre del archivo
-		pdfFileName := fmt.Sprintf("report_Dominica_%s%s.pdf", yesterdayFormatted, config.Config.PdgGenerationTime)
+		pdfFileName := fmt.Sprintf("report_Dominica_%s%s.pdf", todayFormatted, config.Config.PdgGenerationTime)
 
 		ins_log.Tracef(ctx, "this is the name of the pdf file %s", pdfFileName)
 
 		pdfFile, err := os.ReadFile(config.Config.UbicationFiles + pdfFileName)
 		if err != nil {
-			ins_log.Errorf(ctx, "Error al leer el archivo PDF: %s ", err)
+			ins_log.Errorf(ctx, "Error when try to get the pdffile: %s ", err)
 			return
 		}
 
 		encodedPDF := base64.StdEncoding.EncodeToString(pdfFile)
 
-		ins_log.Infof(ctx, "pdf_filed updated")
+		ins_log.Tracef(ctx, "pdf_filed updated ")
 
 		// Establecer conexión con el servidor SMTP
 		client, err := smtp.Dial(smtpHost + ":" + smtpPort)
 		if err != nil {
-			ins_log.Errorf(ctx, "Error al conectar con el servidor SMTP: %s ", err)
+			ins_log.Errorf(ctx, "Error when we try to connect with the SMTP server: %s ", err)
 			return
 		}
 		defer client.Close()
 
-		ins_log.Infof(ctx, "conectted to de smtp server: ")
+		ins_log.Debug(ctx, "conectted to de smtp server")
 
 		// Ejecutar el saludo (HELO)
-		if err := client.Hello("digicelgroup.com"); err != nil {
-			ins_log.Errorf(ctx, "Error al saludar al servidor SMTP: %s ", err)
+		if err := client.Hello(config.Config.SmtpConfig.HostName); err != nil {
+			ins_log.Errorf(ctx, "Error when we try to specify the host name: %s ", err)
 			return
 		}
 
-		ins_log.Infof(ctx, "connected to the group digicelgroup.com")
+		ins_log.Debugf(ctx, "hostname %s", config.Config.SmtpConfig.HostName)
 
 		// Ejecutar el envío del remitente (MAIL FROM)
 		if err := client.Mail(senderEmail); err != nil {
-			ins_log.Errorf(ctx, "Error al enviar el remitente: %s ", err)
+			ins_log.Errorf(ctx, "Failed to send MAIL FROM command: %s", err)
 			return
 		}
-		ins_log.Infof(ctx, "sender updated successfully")
+		ins_log.Debugf(ctx, "sender updated successfully")
 
-		// Ejecutar la entrega a todos los destinatarios (RCPT TO)
+		// Execute the RCPT TO command for all recipients
 		for _, recipient := range recipients {
 			if err := client.Rcpt(recipient); err != nil {
-				ins_log.Errorf(ctx, "Error al entregar al destinatario: %s ", err)
+				ins_log.Errorf(ctx, "Failed to deliver to recipient %s: %s", recipient, err)
 				return
 			}
 		}
-		ins_log.Infof(ctx, "recipients updated successfully")
+		ins_log.Debugf(ctx, "recipients updated successfully")
 
-		//leemos el html
+		// leer el html
 		htmlBodyPath := config.Config.MailInfo.UbicationMessage
 		htmlBody, err := os.ReadFile(htmlBodyPath)
 		if err != nil {
-			ins_log.Errorf(ctx, "error reading html body")
+			ins_log.Errorf(ctx, "Error reading HTML body from %s: %s", htmlBodyPath, err)
 			return
 		}
 
-		// Valores para reemplazar en el HTML
+		// remplazamos los valores del html
 		companyName := config.Config.CompanyName
 		dateYesterday := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
 
 		body := strings.ReplaceAll(string(htmlBody), "[COMPANY_NAME]", companyName)
 		body = strings.ReplaceAll(body, "[DATE_YESTERDAY]", dateYesterday)
 
-		ins_log.Info(ctx, "HTML FILE IS CREATED")
+		ins_log.Tracef(ctx, "HTML body has been created and placeholders have been replaced successfully")
 
 		message := []byte(
 			"To: " + recipientEmails + "\r\n" +
@@ -240,37 +243,43 @@ func sendEmails(ctx context.Context) {
 				"--boundary--\r\n",
 		)
 
-		// Cerrar la sección multipart
+		// Close the multipart section
 		message = append(message, "\r\n--boundary--\r\n"...)
 		message = append(message, "\r\n."...)
-		// Iniciar la escritura del mensaje (DATA)
+
+		// Start writing the message (DATA)
 		w, err := client.Data()
 		if err != nil {
-			ins_log.Errorf(ctx, "Error al iniciar la escritura del mensaje: %s ", err)
+			ins_log.Errorf(ctx, "Error starting the message write: %s", err)
 			return
 		}
 		defer w.Close()
-		ins_log.Infof(ctx, "WRITE MESSAGE UPDATED SUCCESFULY")
-		// Escribir el cuerpo del mensaje
+		ins_log.Tracef(ctx, "Message write started successfully")
+
+		// Write the message body
 		if _, err := w.Write(message); err != nil {
-			ins_log.Errorf(ctx, "Error al escribir el cuerpo del mensaje: %s ", err)
+			ins_log.Errorf(ctx, "Error writing the message body: %s", err)
 			return
 		}
 
-		ins_log.Infof(ctx, "WRITE MESSAGE body UPDATED SUCCESFULY")
+		ins_log.Tracef(ctx, "Message body written successfully")
 
-		// Finalizar la escritura del mensaje
+		// Finish writing the message
 		if err := w.Close(); err != nil {
-			ins_log.Errorf(ctx, "Error al finalizar la escritura del mensaje: %s ", err)
+			ins_log.Errorf(ctx, "Error finishing the message write: %s", err)
 			return
 		}
-		ins_log.Infof(ctx, "finish escritura del mensaje")
-		// QUIT para terminar la sesión SMTP
+		ins_log.Infof(ctx, "Message write finished successfully")
+
+		// QUIT to end the SMTP session
 		if err := client.Quit(); err != nil {
-			ins_log.Errorf(ctx, "Error al finalizar la sesión SMTP: %s ", err)
+			ins_log.Errorf(ctx, "Error ending the SMTP session: %s", err)
 			return
 		}
-		ins_log.Infof(ctx, "smtp session ended")
-		ins_log.Infof(ctx, "Correo electrónico enviado correctamente!")
+		ins_log.Tracef(ctx, "SMTP session ended successfully")
+		ins_log.Infof(ctx, "Email sent successfully!")
 	}
+}
+func end(ctx context.Context) {
+	ins_log.Infof(ctx, "clossing...")
 }
